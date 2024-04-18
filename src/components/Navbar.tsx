40a6f74c9de384e630a06d99f627787e8a6a135e
valay30 <patelvalay308@gmail.com>
2024-04-18 08:41:06 +0530

    almost done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,15 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import { Icons } from './Icons'
 import NavItems from './NavItems'
 import { buttonVariants } from './ui/button'
-
+import Cart from './Cart'
+import { getServerSideUser } from '@/lib/payload-utils'
+import { cookies } from 'next/headers'
+import UserAccountNav from './UserAccountNav'
+import MobileNav from './MobileNav'
 
 const Navbar = async () => {
-  
-  const  user  = null
+  const nextCookies = cookies()
+  const { user } = await getServerSideUser(nextCookies)
 
   return (
     <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
@@ -15,6 +19,7 @@ const Navbar = async () => {
         <MaxWidthWrapper>
           <div className='border-b border-gray-200'>
             <div className='flex h-16 items-center'>
+              <MobileNav />
 
               <div className='ml-4 flex lg:ml-0'>
                 <Link href='/'>
@@ -45,7 +50,17 @@ const Navbar = async () => {
                     />
                   )}
 
-                  
+                  {user ? (
+                    <UserAccountNav user={user} />
+                  ) : (
+                    <Link
+                      href='/sign-up'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Create account
+                    </Link>
+                  )}
 
                   {user ? (
                     <span
@@ -64,7 +79,7 @@ const Navbar = async () => {
                   )}
 
                   <div className='ml-4 flow-root lg:ml-6'>
-                    
+                    <Cart />
                   </div>
                 </div>
               </div>
